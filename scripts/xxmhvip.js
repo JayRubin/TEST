dcf0cdd591ee28f614ce54398f653dae1c11d0b5
Jay Rubin <90993066+JayRubin@users.noreply.github.com>
2021-09-19 09:19:53 +0800

    Update xxmhvip.js

--- scripts/xxmhvip.js
+++ scripts/xxmhvip.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
 #羞羞漫画解锁会员
-^http:\/\/c\.banghankeji\.com\/common\/MemberInfo\/getEntityByCode url script-response-body https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/xxmhvip.js
+^http:\/\/c\.banghankeji\.com\/common\/MemberInfo\/getEntityByCode url script-response-body https://raw.githubusercontent.com/JayRubin/TEST/main/scripts/xxmhvip.js
 hostname = c.banghankeji.com
 下载地址：商店搜索羞羞漫画即可
 使用方法：需要登录才可破解，未登录无法破解
